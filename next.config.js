2f3c9652a0cd126086189c877392f78eb4334d8c
codemasternode <marcinwarzybok@outlook.com>
2019-02-24 12:06:35 +0100

    with SASS erors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 const withSass = require('@zeit/next-sass')
 const withImage = require('next-images')
 const withFonts = require('next-fonts')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withSass(withImage(withFonts))
+module.exports = withFonts(withCSS(withSass(withImage({
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: '[name].[ext]'
+                }
+            }
+        })
+
+        return config
+    }
+}))))
