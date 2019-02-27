9c1d2e92c3e55d6d87dd279849351107c4b284c4
codemasternode <marcinwarzybok@outlook.com>
2019-02-27 23:28:31 +0100

    add stars and reviews

--- components/Popular.js
+++ components/Popular.js
@@ -31,20 +31,50 @@ const PopularItem = ({ item }) => (
                 Kupiono przez {item.bought} osób
             </p>
             <div className="rating-box">
-                <div className="rating-star empty-star">
-
-                </div>
-                <div className="rating-star empty-star">
-
-                </div>
-                <div className="rating-star empty-star">
-
-                </div>
-                <div className="rating-star empty-star">
-
-                </div>
-                <div className="rating-star empty-star">
-
+                <div className={`rating label-bottom color-ok value-${parseInt(item.average_rate)}`}>
+                    <div className="label-value">{item.average_rate.toFixed(2)} ({item.bought})</div>
+                    <div className="star-container">
+                        <div className="star">
+                            <svg className="star-empty">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-empty"></use>
+                            </svg>
+                            <svg className="star-filled">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-filled"></use>
+                            </svg>
+                        </div>
+                        <div className="star">
+                            <svg className="star-empty">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-empty"></use>
+                            </svg>
+                            <svg className="star-filled">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-filled"></use>
+                            </svg>
+                        </div>
+                        <div className="star">
+                            <svg className="star-empty">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-empty"></use>
+                            </svg>
+                            <svg className="star-filled">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-filled"></use>
+                            </svg>
+                        </div>
+                        <div className="star">
+                            <svg className="star-empty">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-empty"></use>
+                            </svg>
+                            <svg className="star-filled">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-filled"></use>
+                            </svg>
+                        </div>
+                        <div className="star">
+                            <svg className="star-empty">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-empty"></use>
+                            </svg>
+                            <svg className="star-filled">
+                                <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="../static/img/star-rating.icons.svg#star-filled"></use>
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
             <button className="card__price">
